5407a49c593fd113e62d539e0a6ffd3a7deb6ca4
hirossan4049 <haruto405329@gmail.com>
2023-06-28 14:50:54 +0900

    Initial Commit

---
+++ DigiSymbols/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DigiSymbols
+//
+//  Created by a on 6/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DigiSymbols/DigiSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiSymbolsApp.swift
+//  DigiSymbols
+//
+//  Created by a on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DigiSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
